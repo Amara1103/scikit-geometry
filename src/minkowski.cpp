26e5ea52ee567d21e4ddd97ae9293d57f7aacf90
Amara0713 <gzx_1103@163.com>
2023-11-18 16:04:19 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -14,13 +14,20 @@ Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, De
     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
 
+template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
+Polygon_with_holes_2 get_minkowski_wrapper(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
+{
+    return get_minkowski(p, q, decomposition1, decomposition2);
+}
+
 void init_minkowski(py::module &m)
 {
     Ptd ptd;
     Sabd sabd;
     auto sub = m.def_submodule("minkowski");
-    sub.def("minkowski_sum_ss", &get_minkowski<Polygon_2, Polygon_2, sabd, sabd>);
-    sub.def("minkowski_sum_ps", &get_minkowski<Polygon_with_holes_2, Polygon_2, ptd, sabd>);
-    sub.def("minkowski_sum_sp", &get_minkowski<Polygon_2, Polygon_with_holes_2, sabd, ptd>);
-    sub.def("minkowski_sum_pp", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, ptd, ptd>);
+
+    sub.def("minkowski_sum_pp", &PYBIND11_OVERLOAD(Polygon_with_holes_2, get_minkowski_wrapper, Polygon_2, Polygon_2, sabd, sabd));
+    sub.def("minkowski_sum_ph", &PYBIND11_OVERLOAD(Polygon_with_holes_2, get_minkowski_wrapper, Polygon_with_holes_2, Polygon_2, ptd, sabd));
+    sub.def("minkowski_sum_hp", &PYBIND11_OVERLOAD(Polygon_with_holes_2, get_minkowski_wrapper, Polygon_2, Polygon_with_holes_2, sabd, ptd));
+    sub.def("minkowski_sum_hh", &PYBIND11_OVERLOAD(Polygon_with_holes_2, get_minkowski_wrapper, Polygon_with_holes_2, Polygon_with_holes_2, ptd, ptd));
 }
