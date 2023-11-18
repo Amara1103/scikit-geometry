f70a988cb1cf7323c881d0225fd3053093f45363
Amara0713 <gzx_1103@163.com>
2023-11-18 18:07:51 +0800

    Polygon

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -1,12 +1,10 @@
 #include "skgeom.hpp"
 #include <CGAL/minkowski_sum_2.h>
-#include <CGAL/Polygon_vertical_decomposition_2.h>
-#include <CGAL/Polygon_triangulation_decomposition_2.h>
-#include <CGAL/Small_side_angle_bisector_decomposition_2.h>
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
+#include <pybind11/functional.h>
+#include <pybind11/chrono.h>
 
-typedef CGAL::Polygon_vertical_decomposition_2<Kernel> Pvd;
-typedef CGAL::Polygon_triangulation_decomposition_2<Kernel> Ptd;
-typedef CGAL::Small_side_angle_bisector_decomposition_2<Kernel> Sabd;
 
 template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
 Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
