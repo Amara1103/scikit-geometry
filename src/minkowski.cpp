ad4142f75482d89de7547e7ef202baef6451cec7
Amara0713 <gzx_1103@163.com>
2023-11-18 18:41:03 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -1,22 +1,58 @@
+// #include "skgeom.hpp"
+// #include <CGAL/minkowski_sum_2.h>
+
+// template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
+// Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
+// {
+//     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
+// }
+
+// void init_minkowski(py::module &m)
+// {
+//     auto sub = m.def_submodule("minkowski");
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, Sabd, Sabd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, Ptd, Sabd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, Ptd, Ptd>);
+// }
+
 #include "skgeom.hpp"
 #include <CGAL/minkowski_sum_2.h>
-#include <pybind11/stl.h>
-#include <pybind11/complex.h>
-#include <pybind11/functional.h>
-#include <pybind11/chrono.h>
-
-
+#include <CGAL/Polygon_2.h>
+#include <CGAL/Polygon_with_holes_2.h>
+#include <type_traits>
 template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
 Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
 {
+    Ptd ptd;
+    Sabd sabd;
+    if (std::is_same<decltype(p), Polygon_2>::value)
+    {
+        // 如果为polygon_2类型，设置分解模式为sabd
+        decomposition1 = sabd;
+    }
+    else
+    {
+        decomposition1 = ptd;
+    }
+    if (std::is_same<decltype(q), Polygon_2>::value)
+    {
+        // 如果为polygon_，设置分解模式为sabd
+        decomposition1 = sabd;
+    }
+    else
+    {
+        decomposition1 = ptd;
+    }
+
     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
 
 void init_minkowski(py::module &m)
 {
     auto sub = m.def_submodule("minkowski");
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, Sabd, Sabd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, Ptd, Sabd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, Ptd, Ptd>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2>);
 }
