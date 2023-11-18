a6d59d8f966117147db4734f5c38b81c95282092
Amara0713 <gzx_1103@163.com>
2023-11-18 15:34:52 +0800

    change pybind module

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -1,33 +1,17 @@
 #include "skgeom.hpp"
 #include <CGAL/minkowski_sum_2.h>
-#include <CGAL/Nef_polyhedron_3.h>
-#include <CGAL/IO/Nef_polyhedron_iostream_3.h>
-#include <CGAL/minkowski_sum_3.h>
 #include <CGAL/Polygon_vertical_decomposition_2.h>
 #include <CGAL/Polygon_triangulation_decomposition_2.h>
 #include <CGAL/Small_side_angle_bisector_decomposition_2.h>
 
-typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
-typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
 typedef CGAL::Polygon_vertical_decomposition_2<Kernel> Pvd;
 typedef CGAL::Polygon_triangulation_decomposition_2<Kernel> Ptd;
 typedef CGAL::Small_side_angle_bisector_decomposition_2<Kernel> Sabd;
 
-template <typename T1, typename T2>
-Polygon_with_holes_2 get_minkowski(T1 p, T2 q)
+template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
+Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
 {
-    return CGAL::minkowski_sum_2(p, q);
-}
-
-Polyhedron_3 polyhedron_minkowski_sum_3(Polyhedron_3 &p, Polyhedron_3 &q)
-{
-    // convert Polyhedron to NEF
-    Nef_polyhedron_3 a(p);
-    Nef_polyhedron_3 b(q);
-    Nef_polyhedron_3 summed = CGAL::minkowski_sum_3(a, b);
-    Polyhedron_3 result;
-    summed.convert_to_polyhedron(result);
-    return result;
+    return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
 
 void init_minkowski(py::module &m)
@@ -35,9 +19,8 @@ void init_minkowski(py::module &m)
     Ptd ptd;
     Sabd sabd;
     auto sub = m.def_submodule("minkowski");
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, sabd>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, sabd, sabd>);
     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, ptd, sabd>);
     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, sabd, ptd>);
     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, ptd, ptd>);
-    sub.def("minkowski_sum", &polyhedron_minkowski_sum_3);
 }
