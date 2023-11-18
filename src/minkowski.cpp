f4669ba4e36abfe1d71d795b68cb0c2918c1b28a
Amara0713 <gzx_1103@163.com>
2023-11-18 14:23:59 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -5,11 +5,13 @@
 #include <CGAL/minkowski_sum_3.h>
 #include <CGAL/Polygon_vertical_decomposition_2.h>
 #include <CGAL/Polygon_triangulation_decomposition_2.h>
+#include <CGAL/Small_side_angle_bisector_decomposition_2.h>
 
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
 typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
-typedef CGAL::Polygon_vertical_decomposition_2<K> Pvd;
-typedef CGAL::Polygon_triangulation_decomposition_2<K> Ptd;
+typedef CGAL::Polygon_vertical_decomposition_2<Kernel> Pvd;
+typedef CGAL::Polygon_triangulation_decomposition_2<Kernel> Ptd;
+typedef CGAL::Small_side_angle_bisector_decomposition_2<Kernel> Sabd;
 
 template <typename T1, typename T2>
 Polygon_with_holes_2 get_minkowski(T1 p, T2 q)
@@ -31,10 +33,11 @@ Polyhedron_3 polyhedron_minkowski_sum_3(Polyhedron_3 &p, Polyhedron_3 &q)
 void init_minkowski(py::module &m)
 {
     Ptd ptd;
+    Sabd sabd;
     auto sub = m.def_submodule("minkowski");
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2,ptd,ptd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, ptd,ptd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2,ptd,ptd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2,ptd,ptd>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, sadb>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, ptd, sabd>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, sabd, ptd>);
+    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, ptd, ptd>);
     sub.def("minkowski_sum", &polyhedron_minkowski_sum_3);
 }
