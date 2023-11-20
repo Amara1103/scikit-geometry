0eea4b3305db52395d9f26563843939de6dd1571
Amara0713 <gzx_1103@163.com>
2023-11-20 10:37:45 +0800

    update

--- src/decomposition.cpp
+++ src/decomposition.cpp
@@ -6,10 +6,8 @@
 void init_decomposition(py::module &m)
 {
     py::class_<Ptd>(m, "Decomposition")
-        .def(py::init<>())
-        .def_readonly_static("Ptd", &Ptd::MY_CONSTANT);
+        .def(py::init<>());
 
     py::class_<Sabd>(m, "Decomposition")
-        .def(py::init<>())
-        .def_readonly_static("Sabd", &Sabd::MY_CONSTANT);
+        .def(py::init<>());
 }
