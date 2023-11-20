1f35217d3477e76bdbff58b92f5793a3f223a233
Amara0713 <gzx_1103@163.com>
2023-11-20 14:45:06 +0800

    update

--- setup.py
+++ setup.py
@@ -125,9 +125,6 @@ ext_modules = [
             'src/principal_component_analysis.cpp',
             'src/minkowski.cpp',
             'src/decomposition.cpp',
-            'src/polyhedron.cpp',
-            'src/aabb_tree.cpp',
-            'src/voronoi_delaunay.cpp',
             'src/optimal_transport.cpp',
             'src/skeleton.cpp',
             'src/inscribed.cpp'
