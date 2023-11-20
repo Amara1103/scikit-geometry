0c13c6218c8ea5a40f0fa8a3d959744a3773f451
Amara0713 <gzx_1103@163.com>
2023-11-20 10:34:49 +0800

    update

--- src/decomposition.cpp
+++ src/decomposition.cpp
@@ -1,8 +1,15 @@
 #include "skgeom.hpp"
-
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
 void init_decomposition(py::module &m)
 {
-    py::class_<Ptd>(m, "Decomposition_ptd");
+    py::class_<Ptd>(m, "Decomposition")
+        .def(py::init<>())
+        .def_readonly_static("Ptd", &Ptd::MY_CONSTANT);
 
-    py::class_<Sabd>(m, "Decomposition_sabd");
+    py::class_<Sabd>(m, "Decomposition")
+        .def(py::init<>())
+        .def_readonly_static("Sabd", &Sabd::MY_CONSTANT);
 }
