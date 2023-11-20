ecfb1e718192ebe75d02692676e3be7517fb56f9
Amara0713 <gzx_1103@163.com>
2023-11-20 14:35:36 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -1,19 +1,6 @@
 #include "skgeom.hpp"
 #include <CGAL/minkowski_sum_2.h>
 
-// Wrap Sabd
-class PySabd : public CGAL::Small_side_angle_bisector_decomposition_2<Kernel>
-{
-public:
-    using CGAL::Small_side_angle_bisector_decomposition_2<Kernel>::Small_side_angle_bisector_decomposition_2;
-};
-
-// Wrap Ptd
-class PyPtd : public CGAL::Polygon_triangulation_decomposition_2<Kernel>
-{
-public:
-    using CGAL::Polygon_triangulation_decomposition_2<Kernel>::Polygon_triangulation_decomposition_2;
-};
 template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
 Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
 {
@@ -23,10 +10,6 @@ Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, De
 void init_minkowski(py::module &m)
 {
     auto sub = m.def_submodule("minkowski");
-    // Bind Sabd
-    py::class_<PySabd>(m, "Sabd");
-    // Bind Ptd
-    py::class_<PyPtd>(m, "Ptd");
     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, Sabd, Sabd>);
     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, Ptd, Sabd>);
     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
