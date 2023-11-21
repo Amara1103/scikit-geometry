c68c070c782bb317096afea0327632c00d703e8c
Amara0713 <gzx_1103@163.com>
2023-11-21 12:32:58 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -15,27 +15,27 @@
 //     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
 //     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, Ptd, Ptd>);
 // }
-
-Polygon_with_holes_2 get_minkowski_1(Polygon_2 p, Polygon_2 q)
+template <typename T1, typename T2>
+Polygon_with_holes_2 get_minkowski_1(T1 p, T2 q)
 {
     return CGAL::minkowski_sum_2(p, q);
 }
-
-Polygon_with_holes_2 get_minkowski_2(Polygon_with_holes_2 p, Polygon_2 q)
+template <typename T1, typename T2>
+Polygon_with_holes_2 get_minkowski_2(T1 p, T2 q)
 {
     Ptd decomposition1;
     Sabd decomposition2;
     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
-
-Polygon_with_holes_2 get_minkowski_3(Polygon_2 p, Polygon_with_holes_2 q)
+template <typename T1, typename T2>
+Polygon_with_holes_2 get_minkowski_3(T1 p, T2 q)
 {
     Sabd decomposition1;
     Ptd decomposition2;
     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
-
-Polygon_with_holes_2 get_minkowski_4(Polygon_with_holes_2 p, Polygon_with_holes_2 q)
+template <typename T1, typename T2>
+Polygon_with_holes_2 get_minkowski_4(T1 p, T2 q)
 {
     Ptd decomposition1;
     Ptd decomposition2;
