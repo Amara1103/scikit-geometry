d46f27ea012cc22e87f5346bf942672a7ba5b34e
Amara0713 <gzx_1103@163.com>
2023-11-21 12:16:50 +0800

    update

--- src/minkowski.cpp
+++ src/minkowski.cpp
@@ -1,17 +1,52 @@
 #include "skgeom.hpp"
 #include <CGAL/minkowski_sum_2.h>
 
-template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
-Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
+// template <typename T1, typename T2, typename Decomposition1, typename Decomposition2>
+// Polygon_with_holes_2 get_minkowski(T1 p, T2 q, Decomposition1 decomposition1, Decomposition2 decomposition2)
+// {
+//     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
+// }
+
+// void init_minkowski(py::module &m)
+// {
+//     auto sub = m.def_submodule("minkowski");
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, Sabd, Sabd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, Ptd, Sabd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
+//     sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, Ptd, Ptd>);
+// }
+
+Polygon_with_holes_2 get_minkowski_1(Polygon_2 p, Polygon_2 q)
 {
+    return CGAL::minkowski_sum_2(p, q);
+}
+
+Polygon_with_holes_2 get_minkowski_2(Polygon_with_holes_2 p, Polygon_2 q)
+{
+    Ptd decomposition1;
+    Sabd decomposition2;
+    return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
+}
+
+Polygon_with_holes_2 get_minkowski_3(Polygon_2 p, Polygon_with_holes_2 q)
+{
+    Sabd decomposition1;
+    Ptd decomposition2;
+    return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
+}
+
+Polygon_with_holes_2 get_minkowski_4(Polygon_with_holes_2 p, Polygon_with_holes_2 q)
+{
+    Ptd decomposition1;
+    Ptd decomposition2;
     return CGAL::minkowski_sum_2(p, q, decomposition1, decomposition2);
 }
 
 void init_minkowski(py::module &m)
 {
     auto sub = m.def_submodule("minkowski");
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_2, Sabd, Sabd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_2, Ptd, Sabd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_2, Polygon_with_holes_2, Sabd, Ptd>);
-    sub.def("minkowski_sum", &get_minkowski<Polygon_with_holes_2, Polygon_with_holes_2, Ptd, Ptd>);
+    sub.def("minkowski_sum", &get_minkowski_1<Polygon_2, Polygon_2>);
+    sub.def("minkowski_sum", &get_minkowski_2<Polygon_with_holes_2, Polygon_2>);
+    sub.def("minkowski_sum", &get_minkowski_3<Polygon_2, Polygon_with_holes_2>);
+    sub.def("minkowski_sum", &get_minkowski_4<Polygon_with_holes_2, Polygon_with_holes_2>);
 }
